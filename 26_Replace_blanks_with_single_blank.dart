a0f18bdaf58d64bb031eebb7d05bf3bd17553d72
AFNAN <afnanp860@gmail.com>
2024-06-20 20:09:25 +0530

    finished

--- 26_Replace_blanks_with_single_blank.dart
+++ 26_Replace_blanks_with_single_blank.dart
@@ -1,23 +1,20 @@
-//26. Write a program to copy its input to its output, 
+//26. Write a program to copy its input to its output,
 //replacing each string of one or more blanks by a single blank.
-
 
 import 'dart:io';
 
-
-int main(){
+int main() {
   String? string;
   stdout.write("Enter some string: ");
-  try{
-    string = stdin.readLineSync()??'';
-  } catch(e){
+  try {
+    string = stdin.readLineSync() ?? '';
+  } catch (e) {
     print("Try again.");
     return 1;
   }
   string = string.replaceAll(RegExp(r'\s+'), ' ');
 
-  print(string);
+  print("After extra blank space removed: $string");
 
-
-return 0;
+  return 0;
 }
